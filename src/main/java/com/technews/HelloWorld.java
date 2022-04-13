f8196ec03b78682b8ce9d52e2d519024ea004d1e
CaptainQuickWit <cppatel720@gmail.com>
2022-04-12 22:04:48 -0400

    commit

---
+++ src/main/java/com/technews/HelloWorld.java
@@ -0,0 +1,8 @@
+//package main.java.com.technews;
+package com.technews;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
